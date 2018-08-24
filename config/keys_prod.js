cce53787397ccb2c2f36f5f4f31e60a8c8ca45f2
Imran Sayed <imranhsayed@gmail.com>
2018-08-24 10:52:24 +0530

    Update

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://onclick:onclick1@ds135421.mlab.com:35421/imran_database',
+	mongoURI: 'mongodb://onclick:onclick1@ds131942.mlab.com:31942/onclick',
 	secretOrKey: process.env.SECRECT_OR_KEY
 };
